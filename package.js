ecc8f992e24290ad8dad85e278b1b0fd3c9f6e34
Charlie DeTar <cfd@media.mit.edu>
2015-02-02 10:44:57 -0700

    Remove obsolete application-configuration
    Fixes issue #1. Thanks @glasser.

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   summary: "Share login among separate domains.",
-  version: "0.0.6",
+  version: "0.0.7",
   name: "admithub:shared-auth",
   git: "https://github.com/AdmitHub/meteor-shared-auth.git"
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('0.9.2');
-  api.use(['application-configuration', 'iron:router@1.0.3', 'templating'], 'client');
+  api.use(['iron:router@1.0.3', 'templating'], 'client');
   api.add_files([
     'lib/routes.js',
     'lib/sharedAuthFrame.html',
